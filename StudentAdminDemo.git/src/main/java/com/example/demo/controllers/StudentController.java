d85a5f5f20a9093e2643d3003dc38f657b342874
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2020-05-01 16:36:42 +0200

    Add files via upload

---
+++ StudentAdminDemo.git/src/main/java/com/example/demo/controllers/StudentController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Student;
+import com.example.demo.repositories.IStudentRepository;
+import com.example.demo.repositories.InMemoryStudentRepositoryImpl;
+import com.example.demo.repositories.StudentRepositoryImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class StudentController
+{
+
+    private IStudentRepository studentRepository;
+
+    public StudentController()
+    {
+        studentRepository = new InMemoryStudentRepositoryImpl();
+    }
+
+    @GetMapping("/")
+    public String index(Model model)
+    {
+        model.addAttribute("students", studentRepository.readAll());
+        return "index";
+    }
+
+    //Very simple prototype of GET-request with parameter
+    //https://www.baeldung.com/spring-request-param
+    //TODO Direct to detailed view of student
+    @GetMapping("/student")
+    @ResponseBody
+    public String getStudentByParameter(@RequestParam int id) {
+        Student stud = studentRepository.read(id);
+        return "The ID is: " + stud.getId()+ " and the cpr is: " + stud.getCpr();
+    }
+}
